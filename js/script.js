4cee4111652c362540b5bfb96070be9c90ff970a
VeroSancha <versanma@gmail.com>
2024-12-29 19:58:54 +0100

    Scroll Horizontal-WIP

--- js/script.js
+++ js/script.js
@@ -14,3 +14,17 @@ jQuery ( '.accordion-cell' ).click (  function  ( ) {
       jQuery ( this ).siblings ( ).toggleClass ( 'collapsed' );
     }
   } );
+
+// scroll horizontal scroll trigger 
+    gsap.registerPlugin(ScrollTrigger);
+
+    let contents= gsap.utils.toArray(".content");
+    
+    gsap.to(contents,{
+      xPercent: -100 * (contents.length - 1),
+      ease: "none",
+      scrollTrigger: {
+        trigger:"#HorScroll",
+        pin: true,
+      scrub: 1,      },
+    });
